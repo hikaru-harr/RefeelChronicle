bf1b93a750d5ff1215d8da3ba85fba505fbcf47a
hikaru-harr <hikaru.a.r.r@gmail.com>
2025-12-08 14:27:33 +0900

    feat: ファイル一覧表示ページを実装し、アップロード機能をレイアウトに分離し、Next.jsの画像リモートパターンを設定しました。

--- apps/web/src/app/files/page.tsx
+++ apps/web/src/app/files/page.tsx
@@ -1,42 +1,49 @@
 "use client";
-import { LoaderCircle, Upload } from "lucide-react";
-import { useRef } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import useUploadFile from "@/features/file/useUploadFile";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface FileItem {
+	key: string;
+	previewUrl: string;
+}
 
 function page() {
-	const inputRef = useRef<HTMLInputElement | null>(null);
-	const { isUploading, handleSelectFiles } = useUploadFile();
-
+	const [files, setFiles] = useState<FileItem[]>([]);
+	useEffect(() => {
+		console.log("useEffect");
+		const init = async () => {
+			const result = await fetch("http://localhost:3000/api/files", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					"Authorization": "Bearer " + localStorage.getItem("jwtToken"),
+				},
+			});
+			const data = await result.json();
+			setFiles(data.files);
+		}
+		init();
+	}, [])
 	return (
-		<div className="flex justify-center">
-			<Button
-				disabled={isUploading}
-				onClick={(e) => {
-					e.stopPropagation();
-					inputRef.current?.click();
-				}}
-				className="h-12 rounded-full w-[250px] cursor-pointer"
-			>
-				{isUploading ? <LoaderCircle className="animate-spin" /> : <Upload />}
-				写真をアップロード
-			</Button>
-
-			<Input
-				ref={inputRef}
-				onChange={(e) => {
-					const files = e.target.files;
-					// TODO: filesがnullの場合エラーにする
-					if (files === null) return;
-					handleSelectFiles(files);
-					e.currentTarget.value = "";
-				}}
-				type="file"
-				className="hidden"
-				multiple
-				accept="image/*"
-			/>
+		<div className="grid grid-cols-3 gap-4">
+			{
+				files.map((file) => (
+					<div
+  key={file.key}
+  className="relative aspect-square overflow-hidden"
+>
+  <Image
+    src={file.previewUrl}
+    alt={file.key}
+    fill
+    className="object-cover"
+    sizes="(min-width: 768px) 33vw, 50vw"
+	unoptimized
+  />
+</div>
+				))
+			}
 		</div>
 	);
 }
